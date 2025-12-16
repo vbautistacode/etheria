20478691f356d3268058b55700582300b61dc29b
vfbautistacode <vitorfbautista@gmail.com>
2025-12-16 13:54:33 -0300

    __init__.py

--- pages/Inicio.py
+++ pages/Inicio.py
@@ -1,3 +1,8 @@
-# pages/Início.py
+# pages/Inicio.py
+import os, sys
+ROOT = os.path.dirname(os.path.dirname(__file__))  # pasta raiz do projeto
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
 from src.app_core import main_inicio
 main_inicio()
--- pages/Numerologia.py
+++ pages/Numerologia.py
@@ -1,3 +1,8 @@
 # pages/Numerologia.py
+import os, sys
+ROOT = os.path.dirname(os.path.dirname(__file__))  # pasta raiz do projeto
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+    
 from src.app_core import main_numerologia
 main_numerologia()
