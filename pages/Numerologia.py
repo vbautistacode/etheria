fba8061ad3d7a53dc3a7ccc2e3774db6a29126ef
vfbautistacode <vitorfbautista@gmail.com>
2025-12-16 14:28:31 -0300

    from src.app_core import main_numerologia

--- pages/Numerologia.py
+++ pages/Numerologia.py
@@ -5,5 +5,5 @@ from pathlib import Path
 # Add parent directory to path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from app import main_numerologia
+from src.app_core import main_numerologia
 main_numerologia()
