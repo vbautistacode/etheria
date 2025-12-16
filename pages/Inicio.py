02cfb38f7a093a515c821315d8e755b38cd6224b
vfbautistacode <vitorfbautista@gmail.com>
2025-12-16 14:27:07 -0300

    src

--- pages/Inicio.py
+++ pages/Inicio.py
@@ -5,5 +5,5 @@ from pathlib import Path
 # Add parent directory to path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from src.app_core import main_inicio
+from app import main_inicio
 main_inicio()
