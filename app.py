cee7b3585183121cfcd4138f2bb9d419e4571bf7
vfbautistacode <vitorfbautista@gmail.com>
2025-12-17 12:11:36 -0300

    st.markdown

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ st.markdown(
     """
     **Etheria** é um espaço simbólico onde ciclos astrológicos e numerológicos se entrelaçam, 
     conectando-se aos arquétipos e revelando padrões que atravessam o tempo e a experiência humana.  
-    Neste ambiente, você pode compreender as relações entre planetas, números e ciclos, 
+    Neste ambiente, você pode compreender as relações entre planetas, números e outras linhas de conhecimento,
     descobrindo sua própria conexão com eles.  
     Cada elemento funciona como uma chave que abre portas para interpretar tanto os movimentos 
     externos do cosmos quanto os fluxos internos da consciência.
 
     Mais do que observar símbolos, aqui você é convidado a vivenciá-los: transformar arquétipos em 
     práticas, práticas em consciência e consciência em presença.  
-    Etheria é uma jornada de autoconhecimento, onde céu e terra se encontram em diálogo constante, 
+    **Etheria** é uma jornada de autoconhecimento, onde céu e terra se encontram em diálogo constante, 
     e cada escolha se integra a uma narrativa maior.
     """
 )
