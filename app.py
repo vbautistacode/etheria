7e07b89db4ebef7568ff91285c4b8a0b907e4644
vfbautistacode <vitorfbautista@gmail.com>
2025-12-17 11:56:29 -0300

    st.markdown

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ st.title("Etheria — Apresentação")
 col1, col2 = st.columns([2.5,1])
 with col1:
     st.video("https://www.youtube.com/watch?v=odibXYdEBPo")
+
+st.markdown(
+    """
+    *Etheria* é o espaço simbólico onde os ciclos astrológicos e numerológicos se encontram.  
+    O **Painel Esotérico** funciona como um mapa interativo: cada planeta, cada número e cada ciclo 
+    são chaves para compreender tanto os movimentos externos quanto os internos.  
+    Aqui, você é convidado a transformar símbolos em práticas, e práticas em consciência.
+    """
+)
