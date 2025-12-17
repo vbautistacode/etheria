99bbd52062338935fdc4af610ad3269215bfe41b
vfbautistacode <vitorfbautista@gmail.com>
2025-12-17 13:36:07 -0300

    col_title, col_help = st.columns([0.40, 0.60])

--- pages/01_Inicio.py
+++ pages/01_Inicio.py
@@ -300,7 +300,7 @@ with c1:
     style = get_planet_style(planet_ast, lang="pt") if use_colors else {"color": "#000000", "icon": ""}
 
     # colunas: título (maior) + botão (estreita)
-    col_title, col_help = st.columns([0.30, 0.70])
+    col_title, col_help = st.columns([0.40, 0.60])
 
     # título com display:flex para garantir alinhamento vertical do conteúdo interno
     with col_title:
