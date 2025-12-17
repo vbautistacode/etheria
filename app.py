4513cdd773be007edd7993b59d25aa613ac22905
vfbautistacode <vitorfbautista@gmail.com>
2025-12-17 04:17:54 -0300

    col1, col2 = st.columns([1,1])  # proporção 2:1

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ st.set_page_config(page_title="Etheria — Apresentação", layout="wide")
 st.title("Etheria — Apresentação")
 
 # URL do vídeo no YouTube
-col1, col2 = st.columns([2,1])  # proporção 2:1
+col1, col2 = st.columns([1,1])  # proporção 2:1
 with col1:
     st.video("https://www.youtube.com/watch?v=odibXYdEBPo")
