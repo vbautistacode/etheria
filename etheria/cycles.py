ee2204c8c057827fd9b0cac99764c7f680850798
vfbautistacode <vitorfbautista@gmail.com>
2026-02-06 16:55:40 -0300

    BASE_YEAR_MAJOR = 2016

--- etheria/cycles.py
+++ etheria/cycles.py
@@ -99,9 +99,9 @@ MAJOR_STEP = -36
 MAJOR_BLOCK = MAJOR_STEP * len(PLANETS_MAJOR)  # 252
 
 # Valores de alinhamento (ajuste conforme sua convenção)
-BASE_YEAR_ASTRO = 2025
-BASE_YEAR_TEOS = 2025
-BASE_YEAR_MAJOR = 2016
+BASE_YEAR_ASTRO = 2026
+BASE_YEAR_TEOS = 2026
+BASE_YEAR_MAJOR = 2017
 
 # -------------------------
 # Conteúdos interpretativos (chaves canônicas EN, valores em pt_BR)
