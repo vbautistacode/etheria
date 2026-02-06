6fbda429fc38c7bf877b5c16f91ca3f5f50c43ea
vfbautistacode <vitorfbautista@gmail.com>
2026-02-06 16:50:29 -0300

    MAJOR_STEP = -36

--- etheria/cycles.py
+++ etheria/cycles.py
@@ -95,13 +95,13 @@ PLANETS_MAJOR: List[str] = [
     "Saturn", "Venus", "Jupiter", "Sun", "Mercury", "Mars", "Moon"
 ]
 
-MAJOR_STEP = -35
+MAJOR_STEP = -36
 MAJOR_BLOCK = MAJOR_STEP * len(PLANETS_MAJOR)  # 252
 
 # Valores de alinhamento (ajuste conforme sua convenção)
-BASE_YEAR_ASTRO = -1
-BASE_YEAR_TEOS = -1
-BASE_YEAR_MAJOR = -1
+BASE_YEAR_ASTRO = 2
+BASE_YEAR_TEOS = 2
+BASE_YEAR_MAJOR = 2
 
 # -------------------------
 # Conteúdos interpretativos (chaves canônicas EN, valores em pt_BR)
