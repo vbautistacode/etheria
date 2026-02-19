e81c3db4ed6328b1414859002a383d7e81652cb6
vfbautistacode <vitorfbautista@gmail.com>
2026-02-19 13:27:13 -0300

    mapa mental

--- pages/11_Biohacking.py
+++ pages/11_Biohacking.py
@@ -70,11 +70,6 @@ if path_local:
     except Exception:
         uploaded = None
 
-if not uploaded:
-    file = st.file_uploader("Enviar imagem do mapa mental (opcional)", type=["png", "jpg", "jpeg"])
-    if file is not None:
-        uploaded = file.read()
-
 # Exibir imagem se houver
 if uploaded:
     st.image(uploaded, use_column_width=True, caption="Mapa mental enviado")
@@ -127,7 +122,7 @@ mermaid_html = f"""
 </style>
 """
 
-st.markdown("#### Versão interativa do mapa (Mermaid)")
+st.markdown("#### Versão interativa do mapa")
 st.components.v1.html(mermaid_html, height=420, scrolling=True)
 
 st.markdown("---")
